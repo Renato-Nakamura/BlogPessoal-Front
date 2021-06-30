b324045e18962e0ded5ee365aed4b3c91002036f
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-06-30 14:22:58 -0300

    tela de inicio

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  nome = environment.nome
+  foto = environment.foto
+  
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+  }
+  sair(){
+    environment.foto=''
+    environment.nome=''
+    environment.token=''
+    this.router.navigate(['/entrar'])
   }
 
 }
